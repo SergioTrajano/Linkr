066a0b4499f0601a5491057aec979312233bd8d1
johana <gomezjohana655@gmail.com>
2022-08-09 08:52:27 -0300

    first commit

---
+++ src/GlobalStyle/index.jsx
@@ -0,0 +1,32 @@
+
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export default createGlobalStyle`
+    ${reset}
+    * {
+        box-sizing: border-box;
+        text-decoration: none;
+        outline: 0;
+        padding: 0;
+        border: 0;
+        margin: 0;
+        word-break: break-word;
+    }
+    
+    button {
+        cursor: pointer;
+    }
+    html, body, #root {
+        background-color:#333333;
+    }
+    :root {
+        --primary-color: #171717;
+        --secondary-color:#FFFFFF;
+        --color-input: #EFEFEF;
+        --color-header:#151515;
+        --color-button: #1877F2;
+        --primary-font: "Lato", sans-serif;
+        --secondary-font: "Oswald", sans-serif;
+    }
+`;
