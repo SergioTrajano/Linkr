3b4eae315631bcfa4138fa894cfefacde0677c68
KayckSchuenck <102394236+KayckSchuenck@users.noreply.github.com>
2022-08-13 13:55:35 -0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,11 @@ import Timeline from "./Timeline";
 import SignUp from "./SignUp";
 import { Login } from "./Login";
 import GlobalStyles from "../GlobalStyles";
-<<<<<<< HEAD
 import { AuthContext } from "../providers/AuthProvider";
 import { useState } from "react";
 import PublicRoute from "./PublicRoute";
 import PrivateRoute from "./PrivateRoute";
-=======
-import HashTag from "./Hashtags/Hashtag.jsx";
 
-export default function App() {
->>>>>>> 4cc0390b98fe0c2a520f845c2519978ac7a522d0
 
 export default function App() {
   const [user, setUser] = useState();
@@ -20,7 +15,6 @@ export default function App() {
 
   return (
     <BrowserRouter>
-<<<<<<< HEAD
       <AuthContext.Provider value={{ user, setUser }}>
         <GlobalStyles />
         <Routes>
@@ -30,22 +24,11 @@ export default function App() {
           </Route>
           <Route element={<PrivateRoute auth={isAuthenticated} />}>
             <Route path="/timeline" element={<Timeline />} />
+            <Route path="/hashtags" element={<HashTag />} />
           </Route>
         </Routes>
       </AuthContext.Provider>
     </BrowserRouter>
-=======
-      <GlobalStyles />
-          <Routes>
 
-          <Route path="/timeline" element={<Timeline />} />
-          <Route path="/hashtags" element={<HashTag />} />
-          </Routes>
- 
-    </BrowserRouter> 
-
-         
-        
->>>>>>> 4cc0390b98fe0c2a520f845c2519978ac7a522d0
   );
 }
