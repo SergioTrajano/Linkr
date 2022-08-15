a0d725e4d9f2fb9d900f8a907f9bfdaaac0771e3
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-08-15 17:59:34 -0300

    delete post

--- src/components/Timeline/Post/index.jsx
+++ src/components/Timeline/Post/index.jsx
@@ -45,6 +45,7 @@ export default function PostCard({
     const toggleEditing = () => {
         setTextEdit(!textEdit);
     };
+    const [modalDisplay, setModalDisplay] = useState("none");
     const config = {
         headers: {
             Authorization: `Bearer ${token}`,
@@ -248,103 +249,127 @@ export default function PostCard({
     }
 
     return (
-        <S.Wrapper >
-
-        <S.PostContainer key={key}>
-        
-            <ProfilePhoto>
-                <img src={pictureURL} alt={legendAlt} />
-                <div className="animation" onClick={postLike}>
-                    <Lottie
-                        options={likeDefaultOptions}
-                        height={65}
-                        width={60}
-                        direction={animationLikeState.direction}
-                        isStopped={animationLikeState.isStopped}
-                    />
+    <>
+        <Modal modalDisplay={modalDisplay}>
+            <div>
+                <p>
+                    Are you sure you want
+                    to delete this post?
+                </p>
+                <div>
+                    <button onClick={() => setModalDisplay("none")}>
+                        No, go back
+                    </button>
+                    <button onClick={() => {
+                        const promise = axios.delete(`${process.env.REACT_APP_API_BASE_URL}/posts/${postId}`, config);
+                        promise.then(() => {
+                            getPosts();
+                            setModalDisplay("flex")
+                        })
+                    }
+                    }>
+                        Yes, delete it
+                    </button>
                 </div>
-                <span data-tip={tooltip}
-                    data-class='classTooltip'
-                    data-arrow-color='white'
-                >
-                    <h6>{like} likes</h6>
-                </span>
-                <ReactTooltip />
-            </ProfilePhoto>
-          
-            <S.PostContentContainer>
+            </div>
+        </Modal>
+        <S.Wrapper >
+            <S.PostContainer key={key}>
 
-            <S.PostUserName>
-             <Link to={`/user/${creatorId}`}>
-                <h3 >{username}</h3>
-             </Link>
-             
-                     <S.IconsContainer  >
-
-                     <TiPencil
-                         onClick={() => {if(name===username) setTextEdit(!textEdit)}}
-                         className="icon-post"
-                     />
-                 
-                     <FaTrash
-                        
-                         className="icon-post"
-                     />
-                    </S.IconsContainer>  
-               
-
-             </S.PostUserName>
-             {textEdit === false ? (
-                    <ReactTagify
-                        tagStyle={tagStyle}
-                        tagClicked={(tag) => {
-                            const tagWithoutHash = tag.replace("#", "");
-                            navigate(`/hashtag/${tagWithoutHash}`);
-                        }}
-                    >
-                        <p style={{color: "#9B9595"}}>{originalBody}</p>
-                    </ReactTagify>
-                ) : (
-                    <S.PostForm onSubmit={(e) => updateBody(e)}>
-                   
-                        <S.PostInput
-                            type="text"
-                            ref={inputRef}
-                            placeholder="Awesome article about #javascript"
-                            onChange={(e) => setBodyValue(e.target.value)}
-                            value={bodyValue}
-                            disabled={isInputDisabled}
-                            onKeyUpCapture={(e) => handleKeyPress(e)}
-                            required
+                <ProfilePhoto>
+                    <img src={pictureURL} alt={legendAlt} />
+                    <div className="animation" onClick={postLike}>
+                        <Lottie
+                            options={likeDefaultOptions}
+                            height={65}
+                            width={60}
+                            direction={animationLikeState.direction}
+                            isStopped={animationLikeState.isStopped}
                         />
-                </S.PostForm>
-                )}
-               
-           
-          
+                    </div>
+                    <span data-tip={tooltip}
+                        data-class='classTooltip'
+                        data-arrow-color='white'
+                    >
+                        <h6>{like} likes</h6>
+                    </span>
+                    <ReactTooltip />
+                </ProfilePhoto>
+
+                <S.PostContentContainer>
+
+                <S.PostUserName>
+                <Link to={`/user/${creatorId}`}>
+                    <h3 >{username}</h3>
+                </Link>
+                
+                        <S.IconsContainer  >
+
+                        <TiPencil
+                            onClick={() => {if(name===username) setTextEdit(!textEdit)}}
+                            className="icon-post"
+                        />
+                    
+                        <FaTrash
+                            onClick={() => {if (name===username) setModalDisplay("flex")}}
+                            className="icon-post"
+                        />
+                        </S.IconsContainer>  
+                
+
+                </S.PostUserName>
+                {textEdit === false ? (
+                        <ReactTagify
+                            tagStyle={tagStyle}
+                            tagClicked={(tag) => {
+                                const tagWithoutHash = tag.replace("#", "");
+                                navigate(`/hashtag/${tagWithoutHash}`);
+                            }}
+                        >
+                            <p style={{color: "#9B9595"}}>{originalBody}</p>
+                        </ReactTagify>
+                    ) : (
+                        <S.PostForm onSubmit={(e) => updateBody(e)}>
+                    
+                            <S.PostInput
+                                type="text"
+                                ref={inputRef}
+                                placeholder="Awesome article about #javascript"
+                                onChange={(e) => setBodyValue(e.target.value)}
+                                value={bodyValue}
+                                disabled={isInputDisabled}
+                                onKeyUpCapture={(e) => handleKeyPress(e)}
+                                required
+                            />
+                    </S.PostForm>
+                    )}
+                
 
 
 
-                <S.PostLinkPreviewContainer href={url} target="_blank">
-                            <S.PostLinkContent>
-                                <span>
-                                    <S.PostLinkTitle>
-                                        {urlTitle}
-                                    </S.PostLinkTitle>
-                                    <S.PostLinkDescription>
-                                        { urlDescription}
-                                    </S.PostLinkDescription>
-                                </span>
-                                <S.PostLinkUrl>{url}</S.PostLinkUrl>
-                            </S.PostLinkContent>
-                            <S.PostLinkImage src={urlImage} alt={urlTitle}/>
-                </S.PostLinkPreviewContainer>
 
-                </S.PostContentContainer>
-        
-        </S.PostContainer >
+
+                    <S.PostLinkPreviewContainer href={url} target="_blank">
+                                <S.PostLinkContent>
+                                    <span>
+                                        <S.PostLinkTitle>
+                                            {urlTitle}
+                                        </S.PostLinkTitle>
+                                        <S.PostLinkDescription>
+                                            { urlDescription}
+                                        </S.PostLinkDescription>
+                                    </span>
+                                    <S.PostLinkUrl>{url}</S.PostLinkUrl>
+                                </S.PostLinkContent>
+                                <S.PostLinkImage src={urlImage} alt={urlTitle}/>
+                    </S.PostLinkPreviewContainer>
+
+                    </S.PostContentContainer>
+
+            </S.PostContainer >
 
         </S.Wrapper>
+    </>
     );
 }
 
@@ -365,4 +390,65 @@ height: 100%;
     }
     
 `;
+
+const Modal = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(255, 255, 255, 0.4);
+    z-index: 2;
+    display: ${props => props.modalDisplay};
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    > div {
+        width: 597px;
+        height: 267px;
+        background-color: #333333;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 40px 130px 70px 130px;
+        border-radius: 20px;
+
+        p {
+            font-family: var(--primary-font);
+            font-size: 34px;
+            line-height: 38px;
+            font-weight: bold;
+            color: var(--secondary-color);
+            margin-bottom: 20px;
+            text-align: center;
+        }
+
+        > div {
+            display: flex;
+            justify-content: space-between;
+            width: 300px;
+
+            > button {
+                width: 134px;
+                height: 37px;
+                border-radius: 5px;
+                border: none;
+                font-size: 18px;
+                line-height: 21px;
+                font-family: var(--primary-font);
+                padding: 10px 20px;
+            }
+
+            > button:first-child {
+                background-color: var(--secondary-color);
+                color: #1877F2;
+            }
+
+             >button:last-child {
+                background-color: #1877F2;
+                color: var(--primary-color);
+            }
+        }
+    }
+`
 
