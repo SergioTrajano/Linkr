31a49b063732c47301d477635262ca606aece930
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-08-15 01:50:35 -0300

    Sergio - feat: add searchBar funcionalities

--- src/components/Header/SearchBar/index.jsx
+++ src/components/Header/SearchBar/index.jsx
@@ -3,12 +3,15 @@ import * as S from "./styles";
 import { AiOutlineSearch } from "react-icons/ai";
 import { DebounceInput } from "react-debounce-input";
 import axios from "axios";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../../../context/userContext";
 
 // eslint-disable-next-line no-unused-vars
 const SearchBar = () => {
+    const { token } = useContext(UserContext);
     const [serachResult, setSearchResult] = useState([]);
+    const [inputValue, setInputValue] = useState("");
  
     return (
         <S.SearchBarContainer>
@@ -16,14 +19,19 @@ const SearchBar = () => {
                 <DebounceInput
                     minLength={3}
                     debounceTimeout={300}
+                    value={inputValue}
                     onChange={event => {
                         const config = {
                             headers: {
                                 Authorization: `Bearer ${token}`
                             }
                         };
-                        const promise = axios.get(`${process.env.REACT_APP_URI}/users?name=${event.target.value}`, config);
-                        promise.then(response => setSearchResult(response.data));
+                        console.log(event.target.value)
+                        const promise = axios.get(`http://localhost:4000/users?name=${event.target.value}`, config);
+                        setInputValue("");
+                        promise.then(response => {
+                            setSearchResult(response.data);
+                        });
                         }
                     }
                     placeholder="Search for people"
@@ -33,9 +41,9 @@ const SearchBar = () => {
                 <AiOutlineSearch className="search-icon" />
             </S.SearchBarContainerInput>
             <S.SearchBarDataResult>
-                {serachResult.map(c => <Link to={`/user/${c.id}`} onClick={() => setSearchResult([])}>
-                    <S.UserImage src={`${c.picture}`}></S.UserImage>
-                    <p>{c.name}</p>
+                {serachResult.map((c, i) => <Link key={i} to={`/user/${c.id}`} onClick={() => {setSearchResult([]); setInputValue("")}}>
+                    <S.UserImage src={`${c.pictureURL}`}></S.UserImage>
+                    <p>{c.username}</p>
                 </Link>)}
             </S.SearchBarDataResult>
         </S.SearchBarContainer>
--- src/components/routes/PrivateRoute.js
+++ src/components/routes/PrivateRoute.js
@@ -3,12 +3,13 @@ import { useContext } from "react";
 import UserContext from "../../context/userContext.js";
 
 export default function PrivateRoute({ auth }) {
-  const { token, setToken, setImage, setName } = useContext(UserContext);
+  const { token, setToken, setImage, setName, setShowHeader } = useContext(UserContext);
   const parseAuth = JSON.parse(auth);
   if (auth && !token) {
     setName(parseAuth.name);
     setToken(parseAuth.token);
     setImage(parseAuth.image);
+    setShowHeader(true);
   }
   if (!auth) return <Navigate to="/" />;
   if (auth && token) return <Outlet />;
